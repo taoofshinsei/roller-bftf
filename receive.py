a825657e6041d246e1f57a58d5efea4d1efdfd18
Genjo <me@example.com>
2017-03-03 19:14:19 +0000

    Simplifying the problem.

--- receive.py
+++ receive.py
@@ -9,19 +9,19 @@ SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', None)
 slack_client = SlackClient(SLACK_TOKEN)
 
-@app.route('/roll', methods=['POST'])
-def inbound():
-    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
-        channel = request.form.get('channel_name')
-        username = request.form.get('user_name')
-        text = request.form.get('text')
-        inbound_message = username + " in " + channel + " says: " + text
-        print(inbound_message)
-        if channel == 'test2':
-            slack_client.api_call("chat.postMessage", channel="#test2", text="Hello from python!")
+#@app.route('/roll', methods=['POST'])
+#def inbound():
+#    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+#        channel = request.form.get('channel_name')
+#        username = request.form.get('user_name')
+#        text = request.form.get('text')
+#        inbound_message = username + " in " + channel + " says: " + text
+#        print(inbound_message)
+#        if channel == 'test2':
+#            slack_client.api_call("chat.postMessage", channel="#test2", text="Hello from python!")
 #            send_message(request.form.get('channel_id'), "Hello " + username + "! It worked!")
-    else:
-        slack_client.api_call("chat.postMessage", channel="#test2", text="Somethin' done gone wrong!")
+#    else:
+#        slack_client.api_call("chat.postMessage", channel="#test2", text="Somethin' done gone wrong!")
 #    return Response(), 200
 
 @app.route('/')
