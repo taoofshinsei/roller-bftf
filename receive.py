0755b6c7d924f1483060f29456a420b6981bd720
Genjo <me@example.com>
2017-03-03 15:57:15 +0000

    Maybe fixed receive.py

--- receive.py
+++ receive.py
@@ -5,12 +5,6 @@ app = Flask(__name__)
 
 SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
 
-def channel_info(channel_id):
-    channel_info = slack_client.api_call("channels.info", channel=channel_id)
-    if channel_info:
-        return channel_info['channel']
-    return None
-
 @app.route('/slack', methods=['POST'])
 def inbound():
     if request.form.get('token') == SLACK_WEBHOOK_SECRET:
@@ -19,12 +13,8 @@ def inbound():
         text = request.form.get('text')
         inbound_message = username + " in " + channel + " says: " + text
         print(inbound_message)
-        detailed_info = channel_info(request.form.get('id'))
-        if detailed_info:
-            print('Latest text from ' + request.form.get('name') + ":")
-            print(detailed_info['latest']['text'])
-        if request.form.get('name') == 'test2':
-            send_message(request.form.get('id'), "Hello " + channel['name'] + "! It worked!")
+        if channel == 'test2':
+            send_message(request.form.get('channel_id'), "Hello " + username + "! It worked!")
     return Response(), 200
 
 @app.route('/', methods=['GET'])
