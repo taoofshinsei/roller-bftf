23fb37a7ebc7e12ea57389983c7593a2af245025
Jeremy Musser <jemusser@indiana.edu>
2017-03-17 17:19:44 -0400

    Refactor complete

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from slackclient import SlackClient
 from flask import Flask, request, Response
-from datetime import datetime
-import die_roller
+from die_roller import Roll
 
 app = Flask(__name__)
 
@@ -10,39 +9,27 @@ SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', None)
 slack_client = SlackClient(SLACK_TOKEN)
 rollerBotName = "RollerBot"
-username = ""
-text = ""
-stringOfResults = ""
+msg = ""
 
 @app.route('/roll', methods=['POST'])
 def inbound():
     if request.form.get('token') == SLACK_WEBHOOK_SECRET:
-        channel = request.form.get('channel_name')
-        username = request.form.get('user_name')
-        text = request.form.get('text')
-        diceResults = die_roller.rollDice(text)
-        if diceResults:
-            sumOfResults = 0;
-            stringOfResults = "@" + username + " rolling " + text + ": `"
-            for i in diceResults[:-1]:
-                sumOfResults += i
-                stringOfResults += "{result} ".format(result=i)
-            sumOfResults += diceResults[-1]
-            if diceResults[-1] >= 0:
-                stringOfResults += "+{result}`".format(result=diceResults[-1])
-            else:
-                stringOfResults += "{result}`".format(result=diceResults[-1])
-            stringOfResults += "\nFinal result of rolling " + text + ": `{result}`".format(result=sumOfResults)
-            slack_client.api_call("chat.postMessage",
-                channel="#" + channel,
-                text=stringOfResults,
-                username=rollerBotName)
-        else:
-            slack_client.api_call("chat.postMessage",
-                channel="#" + channel,
-                text="Could not parse " + text + ".",
-                username=rollerBotName)
-    return Response(), 200
+        channel = request.form.get('channel_name', '')
+        username = request.form.get('user_name', '')
+        text = request.form.get('text', '')
+        try:
+            roller = Roll(text)
+            mod = "`{}{}`".format("+" if roller.mod > 0 else "", roller.mod)
+            dice = ["*{}d{}:* `{}`".format(c, d, " ".join(list(map(str,v))) for c,d,v in roller.results]
+            msg =  "@{} rolls {} {}\n".format(username, " and ".join(dice), mod if roller.mod else '')
+            msg += ">{}".format(roller)
+        except ValueError as exp:
+            msg = str(exp)
+        
+        slack_client.api_call("chat.postMessage", channel="#" + channel,
+                              text=msg, username=rollerBotName)
+        return Response(), 200
+    return Response(), 401
 
 @app.route('/')
 def homepage():
@@ -50,7 +37,7 @@ def homepage():
     <h1>{uname}</h1>
     <p>{results}.</p>
     
-    """.format(uname = username, results=stringOfResults)
+    """.format(uname = username, results=msg)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
